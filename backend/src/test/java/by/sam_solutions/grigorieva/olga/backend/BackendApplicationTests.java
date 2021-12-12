4d26ddeef4cbd4d11562a32b7864e699da17d982
grioool <olga.a.grigorieva@gmail.com>
2021-12-12 22:10:22 +0300

    Initial commit

---
+++ backend/src/test/java/by/sam_solutions/grigorieva/olga/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package by.sam_solutions.grigorieva.olga.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
