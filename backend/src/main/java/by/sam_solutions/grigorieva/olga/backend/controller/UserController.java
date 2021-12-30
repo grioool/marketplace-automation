c927931a5d8ed06f884a656bd2494eb4b2595878
grioool <olga.a.grigorieva@gmail.com>
2021-12-30 15:57:09 +0300

    Backend Purchase and User controllers were fixed

--- backend/src/main/java/by/sam_solutions/grigorieva/olga/backend/controller/UserController.java
+++ backend/src/main/java/by/sam_solutions/grigorieva/olga/backend/controller/UserController.java
@@ -6,13 +6,10 @@ import by.sam_solutions.grigorieva.olga.backend.entity.User;
 import by.sam_solutions.grigorieva.olga.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired
