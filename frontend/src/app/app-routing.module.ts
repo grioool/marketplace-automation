15ac4e5198f320b7400d18262a1285658b924bb4
grioool <olga.a.grigorieva@gmail.com>
2022-01-03 10:44:35 +0300

    Report was fixed

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { PurchaseList } from "./components/purchase-list/purchase-list.component";
 import { UserList } from "./components/user-list/user-list.component";
 import {SupplyList} from "./components/supply-list/supply-list.component";
+import {ReportList} from "./components/report-list/report-list.component";
 
 
 const routes: Routes = [
   {path: "users", component: UserList},
   {path: "purchases", component: PurchaseList},
   {path: "supplies", component: SupplyList},
+  {path: "reports", component: ReportList},
 ];
 
 @NgModule({
