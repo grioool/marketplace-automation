c24901525c0fcba0ae31ab4d2a46cd5a60eee8f4
grioool <olga.a.grigorieva@gmail.com>
2022-01-16 19:06:34 +0300

    Validation was added.

--- backend/src/main/java/by/sam_solutions/grigorieva/olga/backend/dto/UserRegistrationDto.java
+++ backend/src/main/java/by/sam_solutions/grigorieva/olga/backend/dto/UserRegistrationDto.java
@@ -1,27 +1,37 @@
 package by.sam_solutions.grigorieva.olga.backend.dto;
 
 import lombok.Data;
+import org.springframework.security.access.method.P;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 public class UserRegistrationDto {
 
     @NotEmpty
+    @Pattern(regexp="^[(\\w)-]{3,20}")
     private String username;
 
     @NotEmpty
+    @Pattern(regexp="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,10}")
     private String password;
 
     @NotEmpty
+    @Email
     private String email;
 
     @NotEmpty
+    @Pattern(regexp="^[A-Za-zА-Яа-яЁё]{2,20}")
     private String nameCompany;
 
     @NotEmpty
+    @Pattern(regexp="^[0-9]{2,20}")
     private String wbKey;
 
     @NotEmpty
+    @Pattern(regexp="^[0-9]{2,20}")
     private String ozonKey;
 }
