ccd5649c07b2530bbcb5bbacd87268c0ea3d8a25
grioool <olga.a.grigorieva@gmail.com>
2022-01-18 10:48:03 +0300

    Bugs was fixed.

--- frontend/src/app/util/util.ts
+++ frontend/src/app/util/util.ts
@@ -1,5 +1,6 @@
 export const isBlank: (obj: unknown) => boolean =
     (obj: unknown): boolean => obj === undefined || obj === null;
 
+
 export const isPresent: (obj: unknown) => boolean =
     (obj: unknown): boolean => obj !== undefined && obj !== null;
