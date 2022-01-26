e6d4cc339e7938aa1de91ca810fa660dd1e86296
grioool <olga.a.grigorieva@gmail.com>
2022-01-26 19:12:43 +0300

    Refactoring auth.

--- frontend/src/app/http-interceptor/authentication-interceptor.ts
+++ frontend/src/app/http-interceptor/authentication-interceptor.ts
@@ -1,17 +1,26 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {catchError, Observable, throwError} from "rxjs";
 import {environment} from "../../environments/environment";
 import {accessTokenKey} from "../services/auth.service";
+import {NavigationPath} from "../classes/navigation-path";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
 
+    constructor(private router: Router) {
+    }
+
     public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (req.url === environment.apiHost + "/login" || req.url === environment.apiHost + "/token/refresh") return next.handle(req);
+        if (req.url === environment.apiHost + NavigationPath.LOGIN || req.url === environment.apiHost + "/token/refresh") return next.handle(req);
         const authReq = req.clone({
             headers: req.headers.set("Authorization", "Bearer " + localStorage.getItem(accessTokenKey)),
         });
-        return next.handle(authReq);
+        return next.handle(authReq)
+            .pipe(catchError((error: any) => {
+                if(error.status === 403) this.router.navigate([NavigationPath.LOGIN]).then();
+                return throwError(error);
+            }));
     }
 }
