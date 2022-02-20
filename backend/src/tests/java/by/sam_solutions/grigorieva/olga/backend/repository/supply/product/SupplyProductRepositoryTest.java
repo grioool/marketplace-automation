02144445007a9ac62dd1a3ffb41aee8da9c15e7f
grioool <olga.a.grigorieva@gmail.com>
2022-02-21 02:39:56 +0300

    Test version

--- backend/src/tests/java/by/sam_solutions/grigorieva/olga/backend/repository/supply/product/SupplyProductRepositoryTest.java
+++
@@ -1,7 +0,0 @@
-//package by.sam_solutions.grigorieva.olga.backend.repository.supply.product;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class SupplyProductRepositoryTest {
-//
-//}
--- backend/src/tests/java/by/sam_solutions/grigorieva/olga/backend/repository/town/TownRepositoryTest.java
+++
@@ -1,5 +0,0 @@
-package by.sam_solutions.grigorieva.olga.backend.repository.town;
-
-class TownRepositoryTest {
-
-}
